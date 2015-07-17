593160e0658f90444820c0c5c33755751fc91ba8
Qvechar <1119164082@qq.com>
2015-07-18 00:15:36 +0800

    commit from Qvechar

--- src/FirstGit.java
+++ src/FirstGit.java
@@ -2,5 +2,7 @@
 public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("this is a test...");
+		System.out.println("this is a test...");
+		System.out.println("this is a test...");
 	}
 }
